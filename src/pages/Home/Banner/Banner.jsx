337fba665c45636594311cb485946ec3c8f1c6b5
SifatNiloy <sifatniloy18@gmail.com>
2023-10-25 15:35:46 +0600

    frontend practised

---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import img1 from "../../../assets/images/banner/1.jpg";
+import img2 from "../../../assets/images/banner/2.jpg";
+import img3 from "../../../assets/images/banner/3.jpg";
+import img4 from "../../../assets/images/banner/4.jpg";
+import img5 from "../../../assets/images/banner/5.jpg";
+import img6 from "../../../assets/images/banner/6.jpg";
+const Banner = () => {
+  return (
+    <div className="carousel w-full">
+      <div id="slide1" className="carousel-item relative w-full">
+        <img src={img1} />
+        <div className="absolute h-full flex justify-between transform -translate-y-1/2 top-1/2 bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)">
+          <div className="text-white space-y-7 w-1/3">
+            <h2 className="text-6xl font-bold">
+              Affordable price for car servicing
+            </h2>
+            <p>
+              There are many variations of passages of available, but the
+              majority have suffered alteration in some form
+            </p>
+            <div>
+              <button className="btn btn-primary mr-5">Discover More</button>
+              <button className="btn btn-secondary">Latest Project</button>
+            </div>
+          </div>
+          <a href="#slide4" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide2" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide2" className="carousel-item relative w-full">
+        <img src={img2} />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide1" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide3" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide3" className="carousel-item relative w-full">
+        <img src={img3} className="w-full" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide2" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide4" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide4" className="carousel-item relative w-full">
+        <img src={img4} />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide3" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide1" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -1,8 +1,16 @@
 import React from "react";
-
+import logo from "../../../assets/logo.svg";
 const Footer = () => {
   return (
     <footer className="footer p-10 bg-base-200 text-base-content">
+      <aside>
+        <img src={logo} alt="" />
+        <p>
+          ACME Industries Ltd.
+          <br />
+          Providing reliable tech since 1992
+        </p>
+      </aside>
       <nav>
         <header className="footer-title">Services</header>
         <a className="link link-hover">Branding</a>
@@ -23,24 +31,6 @@ const Footer = () => {
         <a className="link link-hover">Privacy policy</a>
         <a className="link link-hover">Cookie policy</a>
       </nav>
-      <form>
-        <header className="footer-title">Newsletter</header>
-        <fieldset className="form-control w-80">
-          <label className="label">
-            <span className="label-text">Enter your email address</span>
-          </label>
-          <div className="relative">
-            <input
-              type="text"
-              placeholder="username@site.com"
-              className="input input-bordered w-full pr-16"
-            />
-            <button className="btn btn-primary absolute top-0 right-0 rounded-l-none">
-              Subscribe
-            </button>
-          </div>
-        </fieldset>
-      </form>
     </footer>
   );
 };
