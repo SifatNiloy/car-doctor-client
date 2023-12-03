ccc3f8baee2d3e9ebdb73ac8a84fd4caa7faef1a
SifatNiloy <sifatniloy18@gmail.com>
2023-12-04 00:21:02 +0600

    createuser form handleling

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,13 +1,19 @@
 import React, { createContext, useState } from "react";
 import app from "../firebase/firebase.config";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
 export const AuthContext = createContext();
-const Auth = getAuth(app);
+const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
   const authInfo = {
     user,
     loading,
+    createUser,
   };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
