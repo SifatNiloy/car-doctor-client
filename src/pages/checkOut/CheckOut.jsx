5a120eea6cc1595bd3f3d9ae7a5692c922f6a8a7
SifatNiloy <sifatniloy18@gmail.com>
2023-12-07 22:48:38 +0600

    checkout form created

--- src/pages/checkOut/CheckOut.jsx
+++ src/pages/checkOut/CheckOut.jsx
@@ -7,6 +7,44 @@ const CheckOut = () => {
   return (
     <div>
       <h2>Book Service: {title} </h2>
+      <form>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="password"
+              className="input input-bordered"
+              required
+            />
+            <label className="label">
+              <a href="#" className="label-text-alt link link-hover">
+                Forgot password?
+              </a>
+            </label>
+          </div>
+        </div>
+        <div className="form-control mt-6">
+          <input
+            className="btn btn-block btn-primary"
+            type="submit"
+            value="Order"
+          />
+        </div>
+      </form>
     </div>
   );
 };
