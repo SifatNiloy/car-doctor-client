827edc9419fb716d43bb2e44f263395aefca8b72
SifatNiloy <sifatniloy18@gmail.com>
2023-12-07 23:13:39 +0600

    checkout form done

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -23,6 +23,7 @@ const router = createBrowserRouter([
         path: "/login",
         element: <Login></Login>,
       },
+
       {
         path: "/signup",
         element: <SignUp />,
--- src/pages/checkOut/CheckOut.jsx
+++ src/pages/checkOut/CheckOut.jsx
@@ -1,14 +1,56 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../AuthProvider/AuthProvider";
 
 const CheckOut = () => {
   const service = useLoaderData();
-  const { title, _id } = service;
+  const { title, _id, price } = service;
+  const { user } = useContext(AuthContext);
+
+  const handleBookService = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const date = form.date.value;
+    const email = user?.email;
+    const order = {
+      customerName: name,
+      email,
+      date,
+      service,
+      price,
+    };
+    console.log(order);
+  };
   return (
     <div>
       <h2>Book Service: {title} </h2>
-      <form>
+      <form onSubmit={handleBookService}>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input
+              type="name"
+              placeholder="Name"
+              className="input input-bordered"
+              defaultValue={user?.displayName}
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Date</span>
+            </label>
+            <input
+              type="date"
+              name="date"
+              placeholder="date"
+              className="input input-bordered"
+              required
+            />
+          </div>
           <div className="form-control">
             <label className="label">
               <span className="label-text">Email</span>
@@ -17,17 +59,19 @@ const CheckOut = () => {
               type="email"
               placeholder="email"
               className="input input-bordered"
+              defaultValue={user?.email}
               required
             />
           </div>
           <div className="form-control">
             <label className="label">
-              <span className="label-text">Password</span>
+              <span className="label-text">Due Amount</span>
             </label>
             <input
-              type="password"
+              type="text"
               placeholder="password"
               className="input input-bordered"
+              defaultValue={"$ " + price}
               required
             />
             <label className="label">
